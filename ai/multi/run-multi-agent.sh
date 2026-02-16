34a7079e5b8e95c7d489dc0b76b63303c2dea9fb
Otavio Rodolfo Piske <angusyoung@gmail.com>
2026-02-16 13:45:27 +0100

    Updated AI examples to work with recent Forage changes

--- ai/multi/run-multi-agent.sh
+++ ai/multi/run-multi-agent.sh
@@ -1,5 +1,5 @@
-camel run multi-agent.camel.yaml application.properties \
-	--dep=mvn:org.apache.camel.forage:forage-agent:1.0-SNAPSHOT \
-	--dep=mvn:org.apache.camel.forage:forage-memory-message-window:1.0-SNAPSHOT \
-	--dep=mvn:org.apache.camel.forage:forage-model-google-gemini:1.0-SNAPSHOT \
-	--dep=mvn:org.apache.camel.forage:forage-model-ollama:1.0-SNAPSHOT
+camel run multi-agent.camel.yaml \
+	--dep=mvn:io.kaoto.forage:forage-agent:1.0-SNAPSHOT \
+	--dep=mvn:io.kaoto.forage:forage-memory-message-window:1.0-SNAPSHOT \
+	--dep=mvn:io.kaoto.forage:forage-model-google-gemini:1.0-SNAPSHOT \
+	--dep=mvn:io.kaoto.forage:forage-model-ollama:1.0-SNAPSHOT
--- ai/single/run-single-agent.sh
+++ ai/single/run-single-agent.sh
@@ -1,4 +1,4 @@
-camel run agent.camel.yaml application.properties \
-	--dep=mvn:org.apache.camel.forage:forage-agent:1.0-SNAPSHOT \
-	--dep=mvn:org.apache.camel.forage:forage-memory-message-window:1.0-SNAPSHOT \
-	--dep=mvn:org.apache.camel.forage:forage-model-ollama:1.0-SNAPSHOT
+camel run agent.camel.yaml \
+	--dep=mvn:io.kaoto.forage:forage-agent:1.0-SNAPSHOT \
+	--dep=mvn:io.kaoto.forage:forage-memory-message-window:1.0-SNAPSHOT \
+	--dep=mvn:io.kaoto.forage:forage-model-ollama:1.0-SNAPSHOT
